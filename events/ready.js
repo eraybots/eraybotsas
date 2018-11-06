7c82d17500e2bb7be8b10a9de017b38976c84118
eraybots <44324407+eraybots@users.noreply.github.com>
2018-11-06 22:05:07 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`Özel Bot`);
+  client.user.setActivity(`Youtube'Da TREnesGamer`, { type: 'WATCHING' });
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Yeniden Kodlandı`);
 };
